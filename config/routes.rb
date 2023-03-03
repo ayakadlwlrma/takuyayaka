6bc4f9aa571c2ff29ba85da5fd8a7ceeaaf95452
ayakadlwlrma <ayaka3015010@gmail.com>
2023-03-03 15:56:51 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,9 @@ Rails.application.routes.draw do
   root 'statics#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
-
-  resources :points
-=======
-  resources :genres
   resources :points do
     resources :loves, only: [:create, :destroy]
-end
->>>>>>> b707825a0f87c69708ff4d1d746492badb4194ac
+  end
   resources :statics
   resources :others
 
