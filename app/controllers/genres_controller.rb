fdff6b461b9759ae01c39fea5c2161e538108dbd
ayakadlwlrma <ayaka3015010@gmail.com>
2023-03-03 12:27:20 +0900

    genreのindex,new

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,24 @@
 class GenresController < ApplicationController
+    def new
+        @genre = Genre.new
+    end
+
+    def create
+        genre = Genre.new(genre_params)
+        if genre.save
+            redirect_to :action => "index"
+        else
+            redirect_to :action => "new"
+        end
+    end
+
+    def index
+        @genres = Genre.all
+    end
+
+    private
+    def genre_params
+        params.require(:genre).permit(:song, :singer)
+    end
+
 end
