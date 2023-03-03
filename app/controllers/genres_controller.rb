b61f51cfba9f40184cbb95ba4519113af6dde2e9
ayakadlwlrma <ayaka3015010@gmail.com>
2023-03-03 14:10:30 +0900

    genreのshow

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -16,6 +16,10 @@ class GenresController < ApplicationController
         @genres = Genre.all
     end
 
+    def show
+        @genre = Genre.find(params[:id])
+    end
+
     private
     def genre_params
         params.require(:genre).permit(:song, :singer)
