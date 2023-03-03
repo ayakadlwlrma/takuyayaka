c5e8fe812326417bec793705a886ca37364a4715
Takuya0404 <125647878+Takuya0404@users.noreply.github.com>
2023-03-03 14:28:32 +0900

    other

--- app/controllers/others_controller.rb
+++ app/controllers/others_controller.rb
@@ -1,2 +1,26 @@
 class OthersController < ApplicationController
+    def index
+        @others = Other.all
+    end
+    def new
+        @other = Other.new
+    end
+    
+    def create
+        other = Other.new(other_params)
+        if othre.save!
+          redirect_to :action => "index"
+        else
+          redirect_to :action => "new"
+        end
+    end
+
+    def show
+        @tweet = Tweet.find(params[:id])
+    end
+    
+    private
+    def other_params
+        params.require(:other).permit(:song, :singer, :text, :voice, :movie)
+    end
 end
