afdf0d9731d456aac570a9606560e8687123a2d4
ayakadlwlrma <ayaka3015010@gmail.com>
2023-03-03 11:46:15 +0900

    pointの新規投稿ページ

---
+++ app/controllers/points_controller.rb
@@ -0,0 +1,15 @@
+class PointsController < ApplicationController
+
+    def new
+        @point = Point.new
+    end
+
+    def create
+        point = Point.new(point_params)
+        if point.save
+            redirect_to :action => "index"
+        else
+            redirect_to :action => "new"
+        end
+    end
+end
