40b4177c50093efee251ff18f23f9b788e860c88
Takuya0404 <125647878+Takuya0404@users.noreply.github.com>
2023-03-09 02:40:11 +0900

    動画POST

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_163140) do
+ActiveRecord::Schema.define(version: 2023_03_08_172414) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "point_id", null: false
@@ -38,7 +38,14 @@ ActiveRecord::Schema.define(version: 2023_03_08_163140) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
- 
+  create_table "loves", force: :cascade do |t|
+    t.integer "point_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["point_id"], name: "index_loves_on_point_id"
+    t.index ["user_id"], name: "index_loves_on_user_id"
+  end
 
   create_table "others", force: :cascade do |t|
     t.string "song"
@@ -58,6 +65,12 @@ ActiveRecord::Schema.define(version: 2023_03_08_163140) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "video"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
