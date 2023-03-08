0b700372d24be66a3157182360ae987116dd472e
Takuya0404 <125647878+Takuya0404@users.noreply.github.com>
2023-03-09 01:13:39 +0900

    gannbaru

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_08_081506) do
+ActiveRecord::Schema.define(version: 2023_03_08_085118) do
 
   create_table "genres", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2023_03_08_081506) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "profile"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
